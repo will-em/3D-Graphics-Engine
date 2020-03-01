6af11ffcdf0562548d9ee732b1d91f59a7675e64
wem <wem@kth.se>
2020-03-01 17:39:32 +0100

    Minor changes.

--- main.py
+++ main.py
@@ -5,14 +5,23 @@ import copy
 
 class Triangle:
     def __init__(self, p):
-        self.p = p #x, y, z
+        self.p = p
 
     def __str__(self):
         return str(self.points[0])
 
+    def getZmean(self):
+        return (self.p[0][2]+self.p[1][2]+self.p[2][2])/3
+
+    def __lt__(self, other):
+        selfzMean = (self.p[0][2]+self.p[1][2]+self.p[2][2])/3
+        otherzMean = (other.p[0][2]+other.p[1][2]+other.p[2][2])/3
+        return otherzMean<selfzMean
+
 class Mesh:
     def __init__(self):
         pass
+
 
 def MatrixMulti(mat, point):
     point = np.insert(point, 3, 1)
@@ -99,6 +108,7 @@ def main():
 
     theta=0
     dz = 8
+    rotSpeed = 0.05
     while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -107,12 +117,14 @@ def main():
 
         screen.fill((0, 0, 0))
 
-        theta+=0.01
+        theta+=rotSpeed
         rotxMat = np.array([[1, 0, 0, 0], [0, np.cos(theta/2), np.sin(theta/2), 0], [0,-np.sin(theta/2), np.cos(theta/2), 0], [0, 0, 0, 1]])
         rotzMat = np.array([[np.cos(theta), np.sin(theta), 0, 0], [-np.sin(theta), np.cos(theta), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
-        for k, tri in enumerate(meshCube.tris):
 
+        meshCube.tris.sort()
+
+        for k, tri in enumerate(meshCube.tris):
             triTrans = copy.deepcopy(tri)
             transPts = []
             projPts = []
@@ -130,29 +142,29 @@ def main():
             vec2 = [transPts[2][:,0]-transPts[0][:,0], transPts[2][:,1]-transPts[0][:,1], transPts[2][:,2]-transPts[0][:,2]]
 
             normal = np.cross(vec1, vec2, axis=0)
-            if np.linalg.norm(normal)!=0:
-                normal = normal/np.linalg.norm(normal)
+            normal = normal/(np.linalg.norm(normal)+1e-16)
 
             diffVec = [triP-cameraP for triP, cameraP in zip(transPts[0], camera)]
 
             light_direction = [0, 0, -1] #Single Direction Light
-            light_direction = light_direction/np.linalg.norm(light_direction)
-            light_dot = np.dot(light_direction, normal)
-
             if np.dot(diffVec, normal)<0:
+                light_direction = light_direction/np.linalg.norm(light_direction)
+                light_dot = np.dot(light_direction, normal)
+                if light_dot<0:
+                    light_dot=0
                 #Draw triangles
                 xs = [projPts[0][:,0]+1, projPts[1][:,0]+1, projPts[2][:,0]+1]
                 ys = [projPts[0][:,1]+1, projPts[1][:,1]+1, projPts[2][:,1]+1]
                 #Scale
                 xs = [i*width/2 for i in xs]
                 ys = [j*height/2 for j in ys]
-                pygame.gfxdraw.filled_trigon(screen, xs[0], ys[0], xs[1], ys[1], xs[2], ys[2], colors[0])
-                pygame.draw.line(screen, (255, 255, 255), (xs[0], ys[0]), (xs[1], ys[1]), line_width)
-                pygame.draw.line(screen, (255, 255, 255), (xs[1], ys[1]), (xs[2], ys[2]), line_width)
-                pygame.draw.line(screen, (255, 255, 255), (xs[2], ys[2]), (xs[0], ys[0]), line_width)
+                pygame.gfxdraw.filled_trigon(screen, xs[0], ys[0], xs[1], ys[1], xs[2], ys[2], (colors[6][0]*light_dot, colors[6][1]*light_dot, colors[6][2]*light_dot))
+                #pygame.draw.line(screen, (255, 255, 255), (xs[0], ys[0]), (xs[1], ys[1]), line_width)
+                #pygame.draw.line(screen, (255, 255, 255), (xs[1], ys[1]), (xs[2], ys[2]), line_width)
+                #pygame.draw.line(screen, (255, 255, 255), (xs[2], ys[2]), (xs[0], ys[0]), line_width)
 
 
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(144)
 
 main()
