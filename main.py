95e618eb50f1b9a2a5cd3881f33ed3fc54a21156
William Emanuelsson <william.z.emanuelsson@gmail.comxcode-select>
2020-03-15 14:22:37 +0100

    Added the Painter's algorithm
    Also added a teapot model

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from pygame import gfxdraw
 import numpy as np
 import copy
+import time
+import operator
 
 class Triangle:
     def __init__(self, p):
@@ -13,25 +15,38 @@ class Triangle:
     def getZmean(self):
         return (self.p[0][2]+self.p[1][2]+self.p[2][2])/3
 
-    def __lt__(self, other):
-        selfzMean = (self.p[0][2]+self.p[1][2]+self.p[2][2])/3
-        otherzMean = (other.p[0][2]+other.p[1][2]+other.p[2][2])/3
-        return otherzMean<selfzMean
+    #def __lt__(self, other):
+    #    selfzMean = (self.p[0][2]+self.p[1][2]+self.p[2][2])/3
+    #    otherzMean = (other.p[0][2]+other.p[1][2]+other.p[2][2])/3
+    #    return otherzMean>selfzMean
 
 class Mesh:
     def __init__(self):
         pass
 
-
+"""
 def MatrixMulti(mat, point):
-    point = np.insert(point, 3, 1)
-    point = np.expand_dims(point, axis=0)
+    point = [point[0], point[1], point[2], 1.0]
+    point = np.array(point)
     newP = point.dot(mat)
-    w = newP[:,3]
-    newP = newP[:, 0:3]
+    w = newP[3]
+    newP = newP[0:3]
     if w!=0:
         newP = newP/w
+    return list(newP)
+"""
 
+def MatrixMulti(A, point):
+    x = [point[0], point[1], point[2], 1.0]
+    #Matrix Mutliplication
+    newP = [x[0]*A[0,0]+ x[1]*A[1,0]+ x[2]*A[2, 0]+ x[3]*A[3, 0],
+    x[0]*A[0,1]+ x[1]*A[1,1]+ x[2]*A[2, 1]+ x[3]*A[3, 1],
+    x[0]*A[0,2]+ x[1]*A[1,2]+ x[2]*A[2, 2]+ x[3]*A[3, 2],
+    x[0]*A[0,3]+ x[1]*A[1,3]+ x[2]*A[2, 3]+ x[3]*A[3, 3]]
+    w = newP[3]
+    newP = newP[0:3]
+    if w!=0:
+        newP[:] = [p/w for p in newP]
     return newP
 
 def loadMesh(path):
@@ -60,32 +75,10 @@ def main():
 
     clock = pygame.time.Clock()
 
+    drawMesh = False
     line_width = 2
-    """
     meshCube = Mesh()
-    #South
-    tri1 = Triangle([np.array([0.0, 0.0, 0.0]), np.array([0.0, 1.0, 0.0]), np.array([1.0, 1.0, 0.0])])
-    tri2 = Triangle([np.array([0.0, 0.0, 0.0]), np.array([1.0, 1.0, 0.0]), np.array([1.0, 0.0, 0.0])])
-    #East
-    tri3 = Triangle([np.array([1.0, 0.0, 0.0]), np.array([1.0, 1.0, 0.0]), np.array([1.0, 1.0, 1.0])])
-    tri4 = Triangle([np.array([1.0, 0.0, 0.0]), np.array([1.0, 1.0, 1.0]), np.array([1.0, 0.0, 1.0])])
-    #North
-    tri5 = Triangle([np.array([1.0, 0.0, 1.0]), np.array([1.0, 1.0, 1.0]), np.array([0.0, 1.0, 1.0])])
-    tri6 = Triangle([np.array([1.0, 0.0, 1.0]), np.array([0.0, 1.0, 1.0]), np.array([0.0, 0.0, 1.0])])
-    #West
-    tri7 = Triangle([np.array([0.0, 0.0, 1.0]), np.array([0.0, 1.0, 1.0]), np.array([0.0, 1.0, 0.0])])
-    tri8 = Triangle([np.array([0.0, 0.0, 1.0]), np.array([0.0, 1.0, 0.0]), np.array([0.0, 0.0, 0.0])])
-    #Top
-    tri9 = Triangle([np.array([0.0, 1.0, 0.0]), np.array([0.0, 1.0, 1.0]), np.array([1.0, 1.0, 1.0])])
-    tri10 = Triangle([np.array([0.0, 1.0, 0.0]), np.array([1.0, 1.0, 1.0]), np.array([1.0, 1.0, 0.0])])
-    #Bottom
-    tri11 = Triangle([np.array([1.0, 0.0, 1.0]), np.array([0.0, 0.0, 1.0]), np.array([0.0, 0.0, 0.0])])
-    tri12 = Triangle([np.array([1.0, 0.0, 1.0]), np.array([0.0, 0.0, 0.0]), np.array([1.0, 0.0, 0.0])])
-
-    meshCube.tris =  [tri1, tri2, tri3, tri4, tri5, tri6, tri7, tri8, tri9, tri10, tri11, tri12]
-    """
-    meshCube = Mesh()
-    meshCube.tris = loadMesh(r"C:\Users\willi\Documents\GitHub\3D-Graphics-Engine\VideoShip.txt")
+    meshCube.tris = loadMesh(r"/Users/William/github/3D-Graphics-Engine/VideoShip.obj")
 
     camera = [0, 0, 0] #Camera position in space
 
@@ -120,26 +113,23 @@ def main():
         theta+=rotSpeed
         rotxMat = np.array([[1, 0, 0, 0], [0, np.cos(theta/2), np.sin(theta/2), 0], [0,-np.sin(theta/2), np.cos(theta/2), 0], [0, 0, 0, 1]])
         rotzMat = np.array([[np.cos(theta), np.sin(theta), 0, 0], [-np.sin(theta), np.cos(theta), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        totMat = rotzMat.dot(rotxMat)
 
-
-        meshCube.tris.sort()
-
+        t = time.time()
+        toDraw = []
         for k, tri in enumerate(meshCube.tris):
-            triTrans = copy.deepcopy(tri)
             transPts = []
             projPts = []
-            for point in triTrans.p:
+            for point in tri.p:
                 #Rotate
-                point=MatrixMulti(rotxMat, point)
-                point=MatrixMulti(rotzMat, point)
+                point=MatrixMulti(totMat, point)
                 #Translate
-                point[:,2]=point[:,2]+dz
+                point[2]=point[2]+dz
 
                 transPts.append(point)
                 projPts.append(MatrixMulti(projMat, point))
-
-            vec1 = [transPts[1][:,0]-transPts[0][:,0], transPts[1][:,1]-transPts[0][:,1], transPts[1][:,2]-transPts[0][:,2]]
-            vec2 = [transPts[2][:,0]-transPts[0][:,0], transPts[2][:,1]-transPts[0][:,1], transPts[2][:,2]-transPts[0][:,2]]
+            vec1 = [transPts[1][0]-transPts[0][0], transPts[1][1]-transPts[0][1], transPts[1][2]-transPts[0][2]]
+            vec2 = [transPts[2][0]-transPts[0][0], transPts[2][1]-transPts[0][1], transPts[2][2]-transPts[0][2]]
 
             normal = np.cross(vec1, vec2, axis=0)
             normal = normal/(np.linalg.norm(normal)+1e-16)
@@ -152,19 +142,29 @@ def main():
                 light_dot = np.dot(light_direction, normal)
                 if light_dot<0:
                     light_dot=0
-                #Draw triangles
-                xs = [projPts[0][:,0]+1, projPts[1][:,0]+1, projPts[2][:,0]+1]
-                ys = [projPts[0][:,1]+1, projPts[1][:,1]+1, projPts[2][:,1]+1]
-                #Scale
-                xs = [i*width/2 for i in xs]
-                ys = [j*height/2 for j in ys]
-                pygame.gfxdraw.filled_trigon(screen, xs[0], ys[0], xs[1], ys[1], xs[2], ys[2], (colors[6][0]*light_dot, colors[6][1]*light_dot, colors[6][2]*light_dot))
-                #pygame.draw.line(screen, (255, 255, 255), (xs[0], ys[0]), (xs[1], ys[1]), line_width)
-                #pygame.draw.line(screen, (255, 255, 255), (xs[1], ys[1]), (xs[2], ys[2]), line_width)
-                #pygame.draw.line(screen, (255, 255, 255), (xs[2], ys[2]), (xs[0], ys[0]), line_width)
 
+                toDraw.append((projPts, light_dot))
 
+        #Painter's algorithm
+        toDraw.sort(key = lambda x: (x[0][0][2]+x[0][1][2]+x[0][2][2])/3, reverse=True)
+
+        for tup in toDraw:
+            projPts = tup[0]
+
+            light_dot = tup[1]
+            #Draw triangles
+            xs = [projPts[0][0]+1, projPts[1][0]+1, projPts[2][0]+1]
+            ys = [projPts[0][1]+1, projPts[1][1]+1, projPts[2][1]+1]
+            #Scale
+            xs = [i*width/2 for i in xs]
+            ys = [j*height/2 for j in ys]
+            pygame.gfxdraw.filled_trigon(screen, int(xs[0]), int(ys[0]), int(xs[1]), int(ys[1]), int(xs[2]), int(ys[2]), (colors[0][0]*light_dot, colors[0][1]*light_dot, colors[0][2]*light_dot))
+            if drawMesh:
+                pygame.draw.line(screen, (255, 255, 255), (xs[0], ys[0]), (xs[1], ys[1]), line_width)
+                pygame.draw.line(screen, (255, 255, 255), (xs[1], ys[1]), (xs[2], ys[2]), line_width)
+                pygame.draw.line(screen, (255, 255, 255), (xs[2], ys[2]), (xs[0], ys[0]), line_width)
+        print((time.time()-t))
         pygame.display.flip()
-        clock.tick(144)
+        clock.tick(60)
 
 main()
